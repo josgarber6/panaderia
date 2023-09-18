a69f424a3244b5c3930d1a2723792abb6c36897e
Dr.Kenobi27 <josgarber6@alum.us.es>
2023-09-18 11:42:49 +0200

    [Renderización login lograda] Ya se puede entrar en la vista users/login

--- panaderia_backend/settings.py
+++ panaderia_backend/settings.py
@@ -86,6 +86,12 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+LOGIN_REDIRECT_URL = '/'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
