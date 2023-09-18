f1180a06e678c726cd60e0e1b17db074d72794cd
Dr.Kenobi27 <josgarber6@alum.us.es>
2023-09-18 11:42:49 +0200

    [Renderización login lograda] Ya se puede entrar en la vista users/login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+# def login(request):
+#   template = loader.get_template('login.html')
+#   return HttpResponse(template.render())
+
+def register(request):
+  template = loader.get_template('register.html')
+  return HttpResponse(template.render())
