ab455c7b6507d592a6a4b81bd60d297efa1e04a4
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-01-09 16:24:15 +0100

    [Registro exitoso]
    El registro se puede realizar correctamente y además se ha reestructurado el proyecto.

--- panaderia-backend/panaderia-backend/asgi.py
+++ panaderia-backend/panaderia-backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for panaderia_backend project.
+ASGI config for panaderia-backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'panaderia_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'panaderia-backend.settings')
 
 application = get_asgi_application()
