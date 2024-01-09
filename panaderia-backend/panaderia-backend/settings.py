7445658e24a7ade1681ce518bb126ff33cff3117
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-01-09 18:52:36 +0100

    [Login exitoso y html provisional] #3
    Ya se puede realizar login y le he dado algo de estilo a la página de home.

--- panaderia-backend/panaderia-backend/settings.py
+++ panaderia-backend/panaderia-backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'authentication',
     'base',
+    'bootstrap5',
 ]
 
 INTERNAL_IPS = [
@@ -134,6 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
