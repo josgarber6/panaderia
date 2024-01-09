1abfd5308376d56e4b7bf349f886e6107114d176
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-01-09 18:52:36 +0100

    [Login exitoso y html provisional] #3
    Ya se puede realizar login y le he dado algo de estilo a la página de home.

--- panaderia-backend/authentication/views.py
+++ panaderia-backend/authentication/views.py
@@ -24,6 +24,10 @@ def login_view(request):
       form = CustomerLoginForm(data=request.POST)
       if form.is_valid():
           # log user in
+          username = form.cleaned_data.get('username')
+          password = form.cleaned_data.get('password')
+          user = authenticate(request=request,username=username, password=password)
+          login(request, user)
           return redirect('/')
   else:
       form = CustomerLoginForm()
@@ -31,13 +35,14 @@ def login_view(request):
 
 def logout_view(request):
   logout(request)
-  return redirect('')
+  return redirect('/')
 
 def change_password_view(request):
   if request.method == 'POST':
       form = PasswordChangeForm(data=request.POST)
       if form.is_valid():
           # change password
+          form.save()
           return redirect('/')
   else:
       form = PasswordChangeForm(user=request.user)
