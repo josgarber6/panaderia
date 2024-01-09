6fde69e6c5249a4ad0441e4e6bfa065592c2c134
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-01-09 18:52:36 +0100

    [Login exitoso y html provisional] #3
    Ya se puede realizar login y le he dado algo de estilo a la página de home.

--- panaderia-backend/panaderia-backend/urls.py
+++ panaderia-backend/panaderia-backend/urls.py
@@ -15,15 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# from django.contrib.auth import views as auth_views
 from django.urls import path, include
-# from authentication import views as user_views
+from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
-    # path('register/', user_views.signup, name='register'),
-    # path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout'),
-    path('auth/', include('authentication.urls'))
+    path('auth/', include('authentication.urls')),
+    path('', views.home, name='home')
 ]
