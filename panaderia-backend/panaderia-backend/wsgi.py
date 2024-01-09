1707e8eba021ee22508911a39610626a331f2e8b
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-01-09 16:24:15 +0100

    [Registro exitoso]
    El registro se puede realizar correctamente y además se ha reestructurado el proyecto.

--- panaderia-backend/panaderia-backend/wsgi.py
+++ panaderia-backend/panaderia-backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for panaderia_backend project.
+WSGI config for panaderia-backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'panaderia_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'panaderia-backend.settings')
 
 application = get_wsgi_application()
