db3dc3b4bd68f4719a162312b6fd734048b2f12e
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-01-15 18:24:10 +0100

    [Cambiar contraseña funcionando] #3
    Los usuarios pueden cambiar su contraseña si lo desean. Los estilos deben cambiarse más adelante

--- panaderia-backend/authentication/urls.py
+++ panaderia-backend/authentication/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('signup/', views.signup_view, name='signup'),
   path('login/', views.login_view, name='login'),
   path('logout/', views.logout_view, name='logout'),
-  path('signup/', views.signup_view, name='signup'),
+  path('change-password/', views.change_password_view, name='change_password'),
 ]
 
