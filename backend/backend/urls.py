575425839bbf3328c4210de329d949006bbe54b4
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-01-28 16:25:52 +0100

    [Navbar funcionando version 1] #1
    Personalización del base.html en curso

---
+++ backend/backend/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from base import views
+from two_factor.urls import urlpatterns as tf_urls
+from two_factor.gateways.twilio.urls import urlpatterns as tf_twilio_urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path('account/', include('authentication.urls')),
+    path('', include(tf_urls)),
+    path('', include(tf_twilio_urls)),
+    path('', views.home, name='home'),
+    path('api/v1.0/', include('product.urls')),
+]
