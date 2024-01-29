36f73658d2262338b9ad37d29f89db421268752e
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-01-29 12:53:12 +0100

    [Licencia uso Two Factor y vista base] #1
    La vista base se ha configurado como el Navbar de Vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import Navbar from './components/Navbar.vue';
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
-  <Navbar/>
+  <HomeView/>
 </template>
 
 <style>
