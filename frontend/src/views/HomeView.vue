a8d7ff81884342f248b6df14df3d42890f4dd09d
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-03-10 22:56:55 +0100

    [Añadir Cart, HomeView y Navbar tests] #8
    Primeras pruebas de los componentes de Vue con algunos ajustes realizados para facilitar los tests

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -114,7 +114,7 @@ export default {
                             for="quantity">Cantidad</label>
                     <input type="number" class="form-control" id="quantity"
                             name="quantity" min="1" value="1">
-                    <button @click="addToCart(product)" class="btn btn-success">Añadir</button>
+                    <button @click="addToCart(product)" id="add-to-cart" class="btn btn-success">Añadir</button>
                   </div>
                 </div>
               </div>
