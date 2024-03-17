4e2ee0cafe97b6c11dace33ae22b146ec2239b8d
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-03-17 22:40:35 +0100

    [feat: Pedidos realizados correctamente faltan pagos] #5
    También se han modificado las URLs del navbar

--- backend/order/viewsets.py
+++ backend/order/viewsets.py
@@ -1,16 +1,23 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Order, OrderItem
 from product.models import Product
 from .serializer import OrderSerializer, OrderItemSerializer
-from django_otp.decorators import otp_required
+from decimal import Decimal
+from two_factor.views.mixins import OTPRequiredMixin
+import stripe
+from decouple import config
 
-class OrderViewSet(viewsets.ModelViewSet):
+class OrderViewSet(OTPRequiredMixin, viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-    # @otp_required
     def create(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response({"detail": "Debe iniciar sesión para realizar un pedido."}, status=status.HTTP_401_UNAUTHORIZED)
+
         data = request.data
         order = Order.objects.create(
             shipping_method=data["shipping_method"],
@@ -24,8 +31,36 @@ class OrderViewSet(viewsets.ModelViewSet):
                 quantity=item["quantity"],
                 order=order
             )
-        return Response(status=status.HTTP_201_CREATED)
+    
+        success_url = config("FRONTEND_BASE_URL") + "orders/payment/success"
+        cancel_url = config("FRONTEND_BASE_URL") + "orders/payment/cancel"
 
-class OrderItemViewSet(viewsets.ModelViewSet):
+        session_data = {
+            "mode": "payment",
+            "client_reference_id": str(order.id),
+            "success_url": success_url,
+            "cancel_url": cancel_url,
+            "line_items": [],
+        }
+
+        for item in order.items.all():
+            session_data["line_items"].append(
+                {
+                    "price_data": {
+                        "unit_amount": int(item.price * Decimal("100")),
+                        "currency": "eur",
+                        "product_data": {
+                            "name": item.product.name,
+                        },
+                    },
+                    "quantity": item.quantity,
+                }
+            )
+
+        session = stripe.checkout.Session.create(**session_data)
+
+        return Response({"message": "Pedido realizado", "session_url": session.url}, status=status.HTTP_201_CREATED)
+
+class OrderItemViewSet(OTPRequiredMixin, viewsets.ModelViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
