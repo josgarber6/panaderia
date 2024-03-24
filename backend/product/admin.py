71870349754b68ec0b30c0a3a0d557281631a243
Dr.Kenobi27 <100224053+josgarber6@users.noreply.github.com>
2024-03-24 18:12:33 +0100

    Revert "Feat/5 orders"

--- backend/product/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product, Category
-
-# Register your models here.
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'price', 'image', 'highlighted', 'stock', 'category']
-
-admin.site.register(Category)
