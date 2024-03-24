2c2ae7e768c04385bacc756a01bcf5ecd236ded0
Dr.Kenobi27 <100224053+josgarber6@users.noreply.github.com>
2024-03-24 18:12:33 +0100

    Revert "Feat/5 orders"

--- backend/authentication/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import Customer
-from rest_framework import serializers
-
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = ['address', 'postal_code', 'user']
--- backend/product/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Product, Category
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'price', 'image', 'highlighted', 'stock', 'category']
-
-admin.site.register(Category)
