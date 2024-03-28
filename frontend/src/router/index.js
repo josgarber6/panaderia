f200a85247996d699c07da5a8d2ed2a582d70538
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-03-28 19:32:16 +0100

    [feat: Añadida administración de pedidos] #21
    Se han retirado dos componentes que daban problemas con el Footer

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -26,10 +26,11 @@ import AdminEditProduct from '@/components/Product/AdminEditProduct.vue'
 import AdminCreateProduct from '@/components/Product/AdminCreateProduct.vue'
 import AdminEditCategory from '@/components/Product/AdminEditCategory.vue'
 import AdminCreateCategory from '@/components/Product/AdminCreateCategory.vue'
+import ListOrderAdmin from '@/components/Order/ListOrderAdmin.vue'
+import AdminEditOrder from '@/components/Order/AdminEditOrder.vue'
 
 import HomeView from '@/views/HomeView.vue'
 import ProductsView from '@/views/ProductsView.vue'
-import MyOrdersView from '@/views/MyOrdersView.vue'
 import AdminProductsView from '@/views/AdminProductsView.vue'
 import AdminCategoriesView from '@/views/AdminCategoriesView.vue'
 
