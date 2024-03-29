af8d94fc9f3c87c0f188856223416ca532a168b9
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-03-29 19:27:34 +0100

    [fix: Quitando imports no usados, limpiando archivos y arreglar viewsets Customer]

--- backend/product/test_selenium.py
+++ backend/product/test_selenium.py
@@ -8,7 +8,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
 import os
 
 
