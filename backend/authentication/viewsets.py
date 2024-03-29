4a01645774079ffd1d5330e6f84cc6d738081c03
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-03-29 18:46:18 +0100

    [fix: Corregido error gravísimo en API Customers]

--- backend/authentication/viewsets.py
+++ backend/authentication/viewsets.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from .models import Customer
 from .serializer import CustomerSerializer
+from two_factor.views.mixins import OTPRequiredMixin
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-class CustomerViewSet(viewsets.ModelViewSet):
+class CustomerViewSet(OTPRequiredMixin, viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser, IsAuthenticated]
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
