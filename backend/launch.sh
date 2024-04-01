1e84f4313fb469c1b0b85164e1b27a7e2e361f84
josgarber6 <josgarber6@alum.us.es>
2024-04-01 16:45:01 +0200

    [feat: Dockerización completa] #23

--- backend/launch.sh
+++ backend/launch.sh
@@ -1,4 +1,14 @@
+mkdir -p logs
+touch logs/gunicorn_access.log
+touch logs/gunicorn_error.log
+
 python3 ./manage.py makemigrations
 python3 ./manage.py migrate
-python3 ./manage.py createsuperuser --noinput
-python3 ./manage.py loaddata fixtures.json
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin123', 'admin@admin.com', 'adminSafePass123') if not User.objects.filter(username='admin123').exists() else None" | python manage.py shell
+
+if [ ! -d "./media/products" ]; then
+    python manage.py loaddata fixtures.json
+fi
+
+gunicorn backend.wsgi:application --config gunicorn.py
